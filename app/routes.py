7d68f536299eb48ec7adccd4c76921c61ee9d751
Maximilian Riegler <akiwa@users.noreply.github.com>
2021-03-04 14:53:14 +0100

    Improve routing names and add HTTP methods

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
 routes = Blueprint("", __name__)
 
@@ -8,21 +8,28 @@ def index():
     return "Welcome to the Task Runner!"
 
 
-@routes.route("/task_started_externally")
-def task_started_externally():
-    return "Route to register externally started processes"
-
-
-@routes.route("/task_finished")
-def task_finished():
-    return "Updates task state to finished"
-
-
-@routes.route("/task_list")
+@routes.route("/tasks", methods=["GET"])
 def task_list():
     return "List of running tasks"
 
 
-@routes.route("/delete_task")
-def delete_task():
+@routes.route("/tasks", methods=["POST"])
+def task_started_internally():
+    return "Route to start process internally"
+
+
+@routes.route("/tasks/external", methods=["POST"])
+def task_started_externally():
+    return "Route to register externally started processes"
+
+
+@routes.route("/tasks/<task_id>", methods=["PUT"])
+def task_finished(task_id):
+    assert task_id == request.view_args['task_id']
+    return "Updates task state to finished"
+
+
+@routes.route("/tasks/<task_id>", methods=["DELETE"])
+def delete_task(task_id):
+    assert task_id == request.view_args['task_id']
     return "Delete finished task after examination"
